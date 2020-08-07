cca2572fd495040d66a229b0a8bd36f893a052cd
Killian Stacey <killianstacey5@gmail.com>
2020-08-07 15:41:27 -0700

    remove redundant lines in init.py file

--- bsoid_py/config/__init__.py
+++ bsoid_py/config/__init__.py
@@ -1,2 +1,2 @@
-from bsoid_py.config.LOCAL_CONFIG import *
-from bsoid_py.config.GLOBAL_CONFIG import *
+# from bsoid_py.config.LOCAL_CONFIG import *  # TODO: these lines don't make sense
+# from bsoid_py.config.GLOBAL_CONFIG import *
