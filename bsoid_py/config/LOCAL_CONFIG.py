f984a35bf1afe37338914302f70a838460019b0c
Aaron Toderash (adt150) <aaron.t@usask.ca>
2020-08-10 01:01:57 -0600

    git ignore and config changes

--- bsoid_py/config/LOCAL_CONFIG.py
+++ bsoid_py/config/LOCAL_CONFIG.py
@@ -1,7 +1,8 @@
 ################### THINGS YOU MAY WANT TO CHANGE ###################
 
-BASE_PATH = '/Users/ahsu/B-SOID/datasets'  # Base directory path.
-TRAIN_FOLDERS = ['/Train1']  # Data folders used to training neural network.
+BASE_PATH = '/home/aaron/Documents/OST-with-DLC/GUI_projects/OST-DLC-projects/pwd-may11-2020-john-howland-2020-05-11'
+
+TRAIN_FOLDERS = ['/training_datasets']  # Data folders used to training neural network.
 PREDICT_FOLDERS = ['/Data1']  # Data folders, can contain the same as training or new data for consistency.
 
 # This version requires the six body parts Snout/Head, Forepaws/Shoulders, Hindpaws/Hips, Tailbase.
@@ -22,19 +23,19 @@ FPS = 60  # Frame-rate of your video,
 COMP = 1  # COMP = 1: Train one classifier for all CSV files; COMP = 0: Classifier/CSV file.
 
 # Output directory to where you want the analysis to be stored
-OUTPUT_PATH = '/Users/ahsu/Desktop/bsoid_py_beta'
+OUTPUT_PATH = '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT'
 # Machine learning model name
-MODEL_NAME = 'c57bl6_n6_30min'
+MODEL_NAME = 'my_first_model'
 
 # Pick a video
-VID_NAME = '/Users/ahsu/B-SOID/datasets/Data1/2019-04-19_09-34-36cut0_30min.mp4'
+VID_NAME = '/home/aaron/Documents/OST-with-DLC/GUI_projects/labelled_videos/002_ratA_inc2_above.mp4'
 # What number would be video be in terms of prediction order? (0=file 1/folder1, 1=file2/folder 1, etc.)
 ID = 0
 # Create a folder to store extracted images, make sure this folder exist.
 # This program will predict labels and print them on these images
-FRAME_DIR = '/Users/ahsu/B-SOID/datasets/Data1/0_30min_10fpsPNGs'
+FRAME_DIR = '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT/frames'
 # In addition, this will also create an entire sample group videos for ease of understanding
-SHORTVID_DIR = '/Users/ahsu/B-SOID/datasets/Data1/examples'
+SHORTVID_DIR = '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT/shortvids'
 
 # IF YOU'D LIKE TO SKIP PLOTTING/CREATION OF VIDEOS, change below plot settings to False
 PLOT_TRAINING = True
