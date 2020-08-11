e1f9f91d21f7196a15ff143aa06a680d15b88e71
Killian Stacey <killianstacey5@gmail.com>
2020-08-11 12:21:37 -0700

    bsoid_py/train.py: change all .format() to f-string and rename variables for clarity. Add TODOS

--- bsoid_py/train.py
+++ bsoid_py/train.py
@@ -4,16 +4,18 @@ we distill information down to 3 dimensions and run pattern recognition.
 Then, we utilize these output and original feature space to train a B-SOiD behavioral model.
 """
 
-import math
-
-import numpy as np
-from sklearn import mixture, svm
+# # # General imports # # #
 from bhtsne import tsne
+from sklearn import mixture, svm
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
+import logging
+import math
+import numpy as np
 
+# # # B-SOID imports # # #
 from bsoid_py.utils.likelihoodprocessing import boxcar_center
 from bsoid_py.utils.visuals import *
 
@@ -26,14 +28,14 @@ def bsoid_tsne(data: list, bodyparts=BODYPARTS, fps=FPS, comp=COMP):
     :param fps: scalar, argument specifying camera frame-rate in LOCAL_CONFIG
     :param comp: boolean (0 or 1), argument to compile data or not in LOCAL_CONFIG
     :return f_10fps: 2D array, features
-    :retrun f_10fps_sc: 2D array, standardized features
+    :return f_10fps_sc: 2D array, standardized features
     :return trained_tsne: 2D array, trained t-SNE space
     """
     win_len = np.int(np.round(0.05 / (1 / fps)) * 2 - 1)
     feats = []
     for m in range(len(data)):
-        logging.info('Extracting features from CSV file {}...'.format(m + 1))
-        dataRange = len(data[m])
+        logging.info(f'Extracting features from CSV file {m+1}...')
+        data_range = len(data[m])
         fpd = data[m][:, 2 * bodyparts.get('Forepaw/Shoulder1'):2 * bodyparts.get('Forepaw/Shoulder1') + 2] - \
               data[m][:, 2 * bodyparts.get('Forepaw/Shoulder2'):2 * bodyparts.get('Forepaw/Shoulder2') + 2]
         cfp = np.vstack(((data[m][:, 2 * bodyparts.get('Forepaw/Shoulder1')] +
@@ -49,13 +51,12 @@ def bsoid_tsne(data: list, bodyparts=BODYPARTS, fps=FPS, comp=COMP):
         chp_pt = np.vstack(([chp[:, 0] - data[m][:, 2 * bodyparts.get('Tailbase')],
                              chp[:, 1] - data[m][:, 2 * bodyparts.get('Tailbase') + 1]])).T
         sn_pt = np.vstack(([data[m][:, 2 * bodyparts.get('Snout/Head')] - data[m][:, 2 * bodyparts.get('Tailbase')],
-                            data[m][:, 2 * bodyparts.get('Snout/Head') + 1] - data[m][:,
-                                                                              2 * bodyparts.get('Tailbase') + 1]])).T
-        fpd_norm = np.zeros(dataRange)
-        cfp_pt_norm = np.zeros(dataRange)
-        chp_pt_norm = np.zeros(dataRange)
-        sn_pt_norm = np.zeros(dataRange)
-        for i in range(1, dataRange):
+                            data[m][:, 2 * bodyparts.get('Snout/Head') + 1] - data[m][:, 2 * bodyparts.get('Tailbase') + 1]])).T
+        fpd_norm = np.zeros(data_range)
+        cfp_pt_norm = np.zeros(data_range)
+        chp_pt_norm = np.zeros(data_range)
+        sn_pt_norm = np.zeros(data_range)
+        for i in range(1, data_range):
             fpd_norm[i] = np.array(np.linalg.norm(fpd[i, :]))
             cfp_pt_norm[i] = np.linalg.norm(cfp_pt[i, :])
             chp_pt_norm[i] = np.linalg.norm(chp_pt[i, :])
@@ -64,10 +65,10 @@ def bsoid_tsne(data: list, bodyparts=BODYPARTS, fps=FPS, comp=COMP):
         sn_cfp_norm_smth = boxcar_center(sn_pt_norm - cfp_pt_norm, win_len)
         sn_chp_norm_smth = boxcar_center(sn_pt_norm - chp_pt_norm, win_len)
         sn_pt_norm_smth = boxcar_center(sn_pt_norm, win_len)
-        sn_pt_ang = np.zeros(dataRange - 1)
-        sn_disp = np.zeros(dataRange - 1)
-        pt_disp = np.zeros(dataRange - 1)
-        for k in range(0, dataRange - 1):
+        sn_pt_ang = np.zeros(data_range - 1)
+        sn_disp = np.zeros(data_range - 1)
+        pt_disp = np.zeros(data_range - 1)
+        for k in range(0, data_range - 1):
             b_3d = np.hstack([sn_pt[k + 1, :], 0])
             a_3d = np.hstack([sn_pt[k, :], 0])
             c = np.cross(b_3d, a_3d)
@@ -84,12 +85,12 @@ def bsoid_tsne(data: list, bodyparts=BODYPARTS, fps=FPS, comp=COMP):
         pt_disp_smth = boxcar_center(pt_disp, win_len)
         feats.append(np.vstack((sn_cfp_norm_smth[1:], sn_chp_norm_smth[1:], fpd_norm_smth[1:],
                                 sn_pt_norm_smth[1:], sn_pt_ang_smth[:], sn_disp_smth[:], pt_disp_smth[:])))
-    logging.info('Done extracting features from a total of {} training CSV files.'.format(len(data)))
+    logging.info(f'Done extracting features from a total of {len(data)} training CSV files.')
     if comp == 0:
         f_10fps = []
         f_10fps_sc = []
         trained_tsne = []
-    for n in range(0, len(feats)):
+    for n in range(0, len(feats)):  # TODO: low: refactor range
         feats1 = np.zeros(len(data[n]))
         for k in range(round(fps / 10) - 1, len(feats[n][0]), round(fps / 10)):
             if k > round(fps / 10) - 1:
@@ -99,9 +100,8 @@ def bsoid_tsne(data: list, bodyparts=BODYPARTS, fps=FPS, comp=COMP):
                                                            axis=1))).reshape(len(feats[0]), 1)), axis=1)
             else:
                 feats1 = np.hstack((np.mean((feats[n][0:4, range(k - round(fps / 10), k)]), axis=1),
-                                    np.sum((feats[n][4:7, range(k - round(fps / 10), k)]), axis=1))).reshape(
-                    len(feats[0]), 1)
-        logging.info('Done integrating features into 100ms bins from CSV file {}.'.format(n + 1))
+                                    np.sum((feats[n][4:7, range(k - round(fps / 10), k)]), axis=1))).reshape(len(feats[0]), 1)
+        logging.info(f'Done integrating features into 100ms bins from CSV file {n+1}.')
         if comp == 1:
             if n > 0:
                 f_10fps = np.concatenate((f_10fps, feats1), axis=1)
@@ -113,27 +113,25 @@ def bsoid_tsne(data: list, bodyparts=BODYPARTS, fps=FPS, comp=COMP):
             scaler.fit(feats1.T)
             feats1_stnd = scaler.transform(feats1.T).T
             f_10fps_sc.append(feats1_stnd)
-            logging.info('Training t-SNE to embed {} instances from {} D '
-                         'into 3 D from CSV file {}...'.format(f_10fps_sc[n].shape[1], f_10fps_sc[n].shape[0],
-                                                               n + 1))
+            logging.info(f'Training t-SNE to embed {f_10fps_sc[n].shape[1]} instances from '
+                         f'{f_10fps_sc[n].shape[0]} D into 3 D from CSV file {n + 1}...')
             trained_tsne_i = tsne(f_10fps_sc[n].T, dimensions=3, perplexity=np.sqrt(f_10fps_sc[n].shape[1]),
                                   theta=0.5, rand_seed=23)
             trained_tsne.append(trained_tsne_i)
             logging.info('Done embedding into 3 D.')
     if comp == 1:
         scaler = StandardScaler()
-        scaler.fit(f_10fps.T)
+        scaler.fit(f_10fps.T)  # TODO: HIGH: variable `f_10fps` referenced before assignment. Error in logic above?
         f_10fps_sc = scaler.transform(f_10fps.T).T
-        logging.info('Training t-SNE to embed {} instances from {} D '
-                     'into 3 D from a total of {} CSV files...'.format(f_10fps_sc.shape[1], f_10fps_sc.shape[0],
-                                                                       len(data)))
+        logging.info(f'Training t-SNE to embed {f_10fps_sc.shape[1]} instances from {f_10fps_sc.shape[0]} D '
+                     'into 3 D from a total of {len(data)} CSV files...')
         trained_tsne = tsne(f_10fps_sc.T, dimensions=3, perplexity=np.sqrt(f_10fps_sc.shape[1]),
                             theta=0.5, rand_seed=23)
         logging.info('Done embedding into 3 D.')
     return f_10fps, f_10fps_sc, trained_tsne, scaler
 
 
-def bsoid_gmm(trained_tsne, comp=COMP, emgmm_params=EMGMM_PARAMS):
+def bsoid_gmm(trained_tsne, comp=COMP, emgmm_params=EMGMM_PARAMS) -> np.ndarray:
     """
     Trains EM-GMM (unsupervised) given learned t-SNE space
     :param trained_tsne: 2D array, trained t-SNE space
@@ -176,24 +174,21 @@ def bsoid_svm(feats, labels, comp=COMP, hldout=HLDOUT, cv_it=CV_IT, svm_params=S
     :return scores: 1D array, cross-validated accuracy
     """
     if comp == 1:
-        feats_train, feats_test, labels_train, labels_test = train_test_split(feats.T, labels.T, test_size=hldout,
-                                                                              random_state=23)
-        logging.info(
-            'Training SVM on randomly partitioned {}% of training data...'.format(
-                (1 - hldout) * 100))
+        feats_train, feats_test, labels_train, labels_test = train_test_split(
+            feats.T, labels.T, test_size=hldout, random_state=23)
+        logging.info(f'Training SVM on randomly partitioned {(1 - hldout) * 100}% of training data...')
         classifier = svm.SVC(**svm_params)
         classifier.fit(feats_train, labels_train)
-        logging.info('Done training SVM mapping {} features to {} assignments.'.format(
-            feats_train.shape, labels_train.shape))
-        logging.info('Predicting randomly sampled (non-overlapped) assignments '
-                     'using the remaining {}%...'.format(HLDOUT * 100))
+        logging.info(f'Done training SVM mapping {feats_train.shape} features to {labels_train.shape} assignments.')
+        logging.info(f'Predicting randomly sampled (non-overlapped) assignments '
+                     'using the remaining {HLDOUT * 100}%...')
         scores = cross_val_score(classifier, feats_test, labels_test, cv=cv_it, n_jobs=-1)
         timestr = time.strftime("_%Y%m%d_%H%M")
         if PLOT_TRAINING:
             np.set_printoptions(precision=2)
             titles_options = [("Non-normalized confusion matrix", None),
                               ("Normalized confusion matrix", 'true')]
-            titlenames = [("counts"), ("norm")]
+            titlenames = [("counts"), ("norm")]  # TODO: low: redundant parentheses
             j = 0
             for title, normalize in titles_options:
                 disp = plot_confusion_matrix(classifier, feats_test, labels_test,
@@ -202,7 +197,7 @@ def bsoid_svm(feats, labels, comp=COMP, hldout=HLDOUT, cv_it=CV_IT, svm_params=S
                 disp.ax_.set_title(title)
                 print(title)
                 print(disp.confusion_matrix)
-                my_file = 'confusion_matrix_{}'.format(titlenames[j])
+                my_file = f'confusion_matrix_{titlenames[j]}'
                 disp.figure_.savefig(os.path.join(OUTPUT_PATH, str.join('', (my_file, timestr, '.svg'))))
                 j += 1
             plt.show()
@@ -210,21 +205,16 @@ def bsoid_svm(feats, labels, comp=COMP, hldout=HLDOUT, cv_it=CV_IT, svm_params=S
         classifier = []
         scores = []
         for i in range(len(feats)):
-            feats_train, feats_test, labels_train, labels_test = train_test_split(feats[i].T, labels[i].T,
-                                                                                  test_size=hldout,
-                                                                                  random_state=23)
-            logging.info(
-                'Training SVM on randomly partitioned {}% of training data...'.format(
-                    (1 - hldout) * 100))
-            classifier = svm.SVC(**svm_params)
+            feats_train, feats_test, labels_train, labels_test = train_test_split(
+                feats[i].T, labels[i].T, test_size=hldout, random_state=23)
+            logging.info(f'Training SVM on randomly partitioned {(1 - hldout) * 100}% of training data...')
+            clf = svm.SVC(**svm_params)
             clf.fit(feats_train, labels_train)
             classifier.append(clf)
-            logging.info(
-                'Done training SVM mapping {} features to {} assignments.'.format(
-                    feats_train.shape, labels_train.shape))
-            logging.info('Predicting randomly sampled (non-overlapped) assignments '
-                         'using the remaining {}%...'.format(HLDOUT * 100))
-            sc = cross_val_score(classifier, feats_test, labels_test, cv=cv_it, n_jobs=-1)
+            logging.info(f'Done training SVM mapping {feats_train.shape} features to {labels_train.shape} assignments.')
+            logging.info(f'Predicting randomly sampled (non-overlapped) assignments using '
+                         f'the remaining {HLDOUT * 100}%...')
+            sc = cross_val_score(classifier, feats_test, labels_test, cv=cv_it, n_jobs=-1)  # TODO: low: `sc` unused variable
             timestr = time.strftime("_%Y%m%d_%H%M")
             if PLOT_TRAINING:
                 np.set_printoptions(precision=2)
@@ -239,12 +229,11 @@ def bsoid_svm(feats, labels, comp=COMP, hldout=HLDOUT, cv_it=CV_IT, svm_params=S
                     disp.ax_.set_title(title)
                     print(title)
                     print(disp.confusion_matrix)
-                    my_file = 'confusion_matrix_clf{}_{}'.format(i, titlenames[j])
+                    my_file = f'confusion_matrix_clf{i}_{titlenames[j]}'
                     disp.figure_.savefig(os.path.join(OUTPUT_PATH, str.join('', (my_file, timestr, '.svg'))))
                     j += 1
                 plt.show()
-    logging.info(
-        'Scored cross-validated SVM performance.'.format(feats_train.shape, labels_train.shape))
+    logging.info('Scored cross-validated SVM performance.'.format(feats_train.shape, labels_train.shape))  # TODO: low: .format() called but variables never used
     return classifier, scores
 
 
@@ -257,7 +246,6 @@ def main(train_folders: list):
     :return classifier: obj, SVM classifier
     :return scores: 1D array, cross-validated accuracy
     """
-    import bsoid_py.utils.likelihoodprocessing
     filenames, training_data, perc_rect = bsoid_py.utils.likelihoodprocessing.main(train_folders)
     f_10fps, f_10fps_sc, trained_tsne, scaler = bsoid_tsne(training_data)
     gmm_assignments = bsoid_gmm(trained_tsne)
