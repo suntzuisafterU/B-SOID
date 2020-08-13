48216d2fb2355df4b2232371d9c7b1c68d4ddde5
Killian Stacey <killianstacey5@gmail.com>
2020-08-13 16:12:12 -0700

    fix importing and versioning for bsoid __init__.py

--- bsoid/__init__.py
+++ bsoid/__init__.py
@@ -1,2 +1,2 @@
-from bsoid_py import *
-__version__ = '0.0.1'  # TODO: med: ensure version number is correct before pushing
+from bsoid import *
+__version__ = '0.0.1'  # TODO: HIGH: ensure version number is correct before pushing
