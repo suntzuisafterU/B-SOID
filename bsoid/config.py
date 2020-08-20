e1ddf3744eafba7d623962af81300f4ac3908620
Killian Stacey <killianstacey5@gmail.com>
2020-08-20 13:31:59 -0700

    solve pathing error found in config; clarify variables in classify

--- bsoid/config.py
+++ bsoid/config.py
@@ -25,14 +25,14 @@ import sys
 from bsoid.util import logger_config
 
 
-debug = 1  # TODO: delete me after debugging and implementation is done.
+debug = 2 # TODO: delete me after debugging and implementation is done.
 
 
 # Fetch the B-SOiD project directory regardless of clone location
 BSOID_BASE_PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if debug: print('BSOID_BASE_PROJECT_PATH:::', BSOID_BASE_PROJECT_PATH)
+if debug>=2: print('BSOID_BASE_PROJECT_PATH:::', BSOID_BASE_PROJECT_PATH)
 default_log_folder_path = Path(BSOID_BASE_PROJECT_PATH, 'logs').absolute()  ##os.path.join(BSOID_BASE_PROJECT_PATH, 'logs')
-if debug: print('default_log_folder_path:::', default_log_folder_path)
+if debug>=2: print('default_log_folder_path:::', default_log_folder_path)
 default_log_file_name = 'default.log'
 config_file_name = 'config.ini'
 # Load up config file
@@ -59,18 +59,19 @@ OST_BASE_PROJECT_PATH = configuration['PATH']['OST_BASE_PROJECT_PATH']  # 'previ
 
 ########################################################################################################################
 
-if debug: print("configuration.get('LOGGING', 'LOG_FILE_NAME'):::", configuration.get('LOGGING', 'LOG_FILE_NAME'))
-if debug: print('OST PATH:::', configuration.get('PATH', 'OSTPATH', fallback=None))
+if debug<2: print("configuration.get('LOGGING', 'LOG_FILE_NAME'):::", configuration.get('LOGGING', 'LOG_FILE_NAME'))
+if debug<2: print('OST PATH:::', configuration.get('PATH', 'OSTPATH', fallback=None))
 
 # Resolve logger variables
-config_file_log_folder_path = os.path.abspath(configuration.get('LOGGING', 'LOG_FILE_FOLDER_PATH', fallback=default_log_folder_path))
-if debug: print('config_file_log_folder_path:::', config_file_log_folder_path)
+config_file_log_folder_path = configuration.get('LOGGING', 'LOG_FILE_FOLDER_PATH')
+config_file_log_folder_path = config_file_log_folder_path if config_file_log_folder_path else default_log_folder_path
+if debug>=2: print('config_file_log_folder_path:::', config_file_log_folder_path)
 
 config_file_name = configuration.get('LOGGING', 'LOG_FILE_NAME', fallback=default_log_file_name)
-if debug: print('config_file_name:::', config_file_name)
+if debug>=2: print('config_file_name:::', config_file_name)
 
 log_file_file_path = str(Path(config_file_log_folder_path, config_file_name).absolute())
-if debug: print('log_file_file_path AKA os.path.join(config_file_log_folder_path, config_file_name):::', log_file_file_path)
+if debug>=2: print('log_file_file_path AKA os.path.join(config_file_log_folder_path, config_file_name):::', log_file_file_path)
 
 assert os.path.isdir(config_file_log_folder_path), f'Path does not exist: {config_file_log_folder_path}'
 
@@ -89,47 +90,6 @@ bsoid_logger = logger_config.create_generic_logger(
 
 ########################################################################################################################
 
-# BASE_PATH = '/home/aaron/Documents/OST-with-DLC/GUI_projects/OST-DLC-projects/pwd-may11-2020-john-howland-2020-05-11'
-BASE_PATH = os.path.join('C:\\', 'Users', 'killian', 'projects', 'OST-with-DLC', 'pwd-may11-2020-john-howland-2020-05-11')
-
-# Output directory to where you want the analysis to be stored
-# OUTPUT_PATH = os.path.join(OST_BASE_PROJECT_PATH, 'B-SOID', 'OUTPUT')  # '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT'
-OUTPUT_PATH = os.path.join('C:\\', 'Users', 'killian', 'Pictures')
-
-MODEL_NAME = configuration.get('APP', 'OUTPUT_MODEL_NAME')  # Machine learning model name
-
-# TODO: med: for TRAIN_FOLDERS & PREDICT_FOLDERS, change path resolution from inside functional module to inside this config file
-# Data folders used to training neural network.
-TRAIN_FOLDERS = [os.path.sep+'training_datasets', ]
-
-# Data folders, can contain the same as training or new data for consistency.
-PREDICT_FOLDERS = [os.path.sep+'Data1', ]
-
-# Create a folder to store extracted images, MAKE SURE THIS FOLDER EXISTS.  # TODO: med: add in a runtime check that folder exists
-FRAME_DIR = os.path.join(OST_BASE_PROJECT_PATH, 'B-SOID', 'OUTPUT', 'frames')  # '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT/frames'
-
-# Create a folder to store created video snippets/group, MAKE SURE THIS FOLDER EXISTS.  # TODO: med: add in a runtime check that folder exists
-# Create a folder to store extracted images, make sure this folder exist.
-#   This program will predict labels and print them on these images
-# In addition, this will also create an entire sample group videos for ease of understanding
-SHORTVID_DIR = os.path.join(OST_BASE_PROJECT_PATH, 'B-SOID', 'OUTPUT', 'shortvids')  # '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT/shortvids'
-
-# Now, pick an example video that corresponds to one of the csv files from the PREDICT_FOLDERS
-VID_NAME = os.path.join(OST_BASE_PROJECT_PATH, 'GUI_projects', 'labelled_videos', '002_ratA_inc2_above.mp4')  # '/home/aaron/Documents/OST-with-DLC/GUI_projects/labelled_videos/002_ratA_inc2_above.mp4'
-
-
-# This version requires the six body parts Snout/Head, Forepaws/Shoulders, Hindpaws/Hips, Tailbase.
-BODYPARTS = {
-    'Snout/Head': 0,
-    'Neck': None,
-    'Forepaw/Shoulder1': 1,
-    'Forepaw/Shoulder2': 2,
-    'Bodycenter': None,
-    'Hindpaw/Hip1': 3,
-    'Hindpaw/Hip2': 4,
-    'Tailbase': 5,
-    'Tailroot': None,
-}
 
 
 ##############################################################################################################
@@ -194,7 +154,7 @@ EMGMM_PARAMS = {
 }
 # Multi-class support vector machine classifier params
 SVM_PARAMS = {
-    'C': configuration.getint('SVM', 'C'),
+    'C': configuration.getfloat('SVM', 'C'),
     'gamma': configuration.getfloat('SVM', 'gamma'),
     'probability': configuration.getboolean('SVM', 'probability'),
     'verbose': configuration.getint('SVM', 'verbose'),
@@ -224,11 +184,7 @@ TSNE_PARAMS = {
     'random_state': configuration.getint('MODEL', 'RANDOM_STATE'),
 }
 
-
-
-
 # TODO: HIGH: after all config parsing done, write checks that will run ON IMPORT to ensure folders exist :)
-
 
 
 ########################################################################################################################
@@ -249,3 +205,48 @@ ID = 0
 # IF YOU'D LIKE TO SKIP PLOTTING/CREATION OF VIDEOS, change below plot settings to False
 PLOT_TRAINING = True
 GEN_VIDEOS = True
+
+
+# BASE_PATH = '/home/aaron/Documents/OST-with-DLC/GUI_projects/OST-DLC-projects/pwd-may11-2020-john-howland-2020-05-11'
+BASE_PATH = os.path.join('C:\\', 'Users', 'killian', 'projects', 'OST-with-DLC', 'pwd-may11-2020-john-howland-2020-05-11')
+
+# Output directory to where you want the analysis to be stored
+# OUTPUT_PATH = os.path.join(OST_BASE_PROJECT_PATH, 'B-SOID', 'OUTPUT')  # '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT'
+OUTPUT_PATH = os.path.join('C:\\', 'Users', 'killian', 'Pictures')
+
+MODEL_NAME = configuration.get('APP', 'OUTPUT_MODEL_NAME')  # Machine learning model name
+
+# TODO: med: for TRAIN_FOLDERS & PREDICT_FOLDERS, change path resolution from inside functional module to inside this config file
+# Data folders used to training neural network.
+TRAIN_FOLDERS = [os.path.sep+'training_datasets', ]
+
+# Data folders, can contain the same as training or new data for consistency.
+PREDICT_FOLDERS = [os.path.sep+'Data1', ]
+
+# Create a folder to store extracted images, MAKE SURE THIS FOLDER EXISTS.  # TODO: med: add in a runtime check that folder exists
+FRAME_DIR = os.path.join(OST_BASE_PROJECT_PATH, 'B-SOID', 'OUTPUT', 'frames')  # '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT/frames'
+
+# Create a folder to store created video snippets/group, MAKE SURE THIS FOLDER EXISTS.  # TODO: med: add in a runtime check that folder exists
+# Create a folder to store extracted images, make sure this folder exist.
+#   This program will predict labels and print them on these images
+# In addition, this will also create an entire sample group videos for ease of understanding
+SHORTVID_DIR = os.path.join(OST_BASE_PROJECT_PATH, 'B-SOID', 'OUTPUT', 'shortvids')  # '/home/aaron/Documents/OST-with-DLC/B-SOID/OUTPUT/shortvids'
+# assert os.path.isdir(SHORTVID_DIR), f'`SHORTVID` dir. (value={SHORTVID_DIR}) must exist for runtime but does not.'
+
+# Now, pick an example video that corresponds to one of the csv files from the PREDICT_FOLDERS
+
+VID_NAME = os.path.join(OST_BASE_PROJECT_PATH, 'GUI_projects', 'labelled_videos', '002_ratA_inc2_above.mp4')  # '/home/aaron/Documents/OST-with-DLC/GUI_projects/labelled_videos/002_ratA_inc2_above.mp4'
+
+
+# This version requires the six body parts Snout/Head, Forepaws/Shoulders, Hindpaws/Hips, Tailbase.
+BODYPARTS = {
+    'Snout/Head': 0,
+    'Neck': None,
+    'Forepaw/Shoulder1': 1,
+    'Forepaw/Shoulder2': 2,
+    'Bodycenter': None,
+    'Hindpaw/Hip1': 3,
+    'Hindpaw/Hip2': 4,
+    'Tailbase': 5,
+    'Tailroot': None,
+}
