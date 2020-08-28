7ae4e05375a200e99a41e1f8b1e2ee9118e903c6
Killian Stacey <killianstacey5@gmail.com>
2020-08-28 12:16:23 -0700

    likelihoodproc: refactor for clarity, vars and whitespace

--- bsoid/util/likelihoodprocessing.py
+++ bsoid/util/likelihoodprocessing.py
@@ -15,6 +15,8 @@ import warnings
 
 from bsoid import config
 
+logger = config.bsoid_logger
+
 
 ########################################################################################################################
 
@@ -25,8 +27,8 @@ def boxcar_center(a, n) -> np.ndarray:
     :param n: TODO
     :return: TODO
     """
-    a1 = pd.Series(a)
-    moving_avg = np.array(a1.rolling(window=n, min_periods=1, center=True).mean())
+    a_as_Series = pd.Series(a)
+    moving_avg = np.array(a_as_Series.rolling(window=n, min_periods=1, center=True).mean())
 
     return moving_avg
 
@@ -70,7 +72,7 @@ def get_filenames(folder):
     return replacement_func(folder)
 
 
-def get_filenames_csvs_from_folders_recursively_in_basepath(folder: str):
+def get_filenames_csvs_from_folders_recursively_in_basepath(folder: str) -> List:
     """
     Get_filenames() makes the assumption that the folder is in BASEPATH; however, it is an obfuscated assumption
     and bad. A new function that DOES NOT RESOLVE PATH IMPLICITLY WITHIN should be created and used.
@@ -78,10 +80,10 @@ def get_filenames_csvs_from_folders_recursively_in_basepath(folder: str):
     :return:
     """
     path_to_check_for_csvs = f'{config.BASE_PATH}{folder}{os.path.sep}**{os.path.sep}*.csv'
-    config.bsoid_logger.debug(f'Path that is being checked with "glob": {path_to_check_for_csvs}')
+    logger.debug(f'Path that is being checked with using glob selection: {path_to_check_for_csvs}')
     filenames = glob.glob(path_to_check_for_csvs, recursive=True)
     sort_list_nicely_in_place(filenames)
-    config.bsoid_logger.info(f'List of files found: {filenames}. Total files found : {len(filenames)}.')
+    logger.info(f'List of files found: {filenames}. Total files found : {len(filenames)}.')
     return filenames
 
 
@@ -100,17 +102,17 @@ def import_csvs_data_from_folders_in_BASEPATH_and_process_data(folders: list) ->
     for idx_folder, folder in enumerate(folders):  # Loop through folders
         filenames_found_in_current_folder = get_filenames_csvs_from_folders_recursively_in_basepath(folder)
         for idx_filename, filename in enumerate(filenames_found_in_current_folder):
-            config.bsoid_logger.info(f'Importing CSV file {idx_filename+1} from folder {idx_folder+1}')
+            logger.info(f'Importing CSV file {idx_filename+1} from folder {idx_folder+1}')
             df_current_file = pd.read_csv(filename, low_memory=False)
             curr_df_filt, perc_rect = adaptive_filter_data(df_current_file)
-            config.bsoid_logger.info(f'Done preprocessing (x,y) from file #{idx_filename+1}, folder #{idx_folder+1}.')
+            logger.info(f'Done preprocessing (x,y) from file #{idx_filename+1}, folder #{idx_folder+1}.')
             raw_data_list.append(df_current_file)
             perc_rect_list.append(perc_rect)
             data_list.append(curr_df_filt)
         file_names_list.append(filenames_found_in_current_folder)
-        config.bsoid_logger.info(f'Processed {len(filenames_found_in_current_folder)} CSV files from folder: {folder}')
+        logger.info(f'Processed {len(filenames_found_in_current_folder)} CSV files from folder: {folder}')
     data_array: np.ndarray = np.array(data_list)
-    config.bsoid_logger.info(f'Processed a total of {len(data_list)} CSV files and '
+    logger.info(f'Processed a total of {len(data_list)} CSV files and '
                              f'compiled into a {data_array.shape} data list.')
     return file_names_list, data_array, perc_rect_list
 
@@ -118,7 +120,7 @@ def import_csvs_data_from_folders_in_BASEPATH_and_process_data(folders: list) ->
 def adaptive_filter_data(df_input: pd.DataFrame) -> Tuple[np.ndarray, List]:
     """
     TODO: low: purpose
-    :param df_input:
+    :param df_input: (DataFrame) TODO
     :param currdf: object, csv data frame
     :return currdf_filt: 2D array, filtered data
     :return perc_rect: 1D array, percent filtered per BODYPART
@@ -138,23 +140,24 @@ def adaptive_filter_data(df_input: pd.DataFrame) -> Tuple[np.ndarray, List]:
         elif currdf[0][header_idx] == "y":
             y_index.append(header_idx)
         else: pass  # TODO: low: should this be failing silently?
-    config.bsoid_logger.info('Extracting likelihood value...')
+
+    logger.info('Extracting likelihood value...')
     curr_df1 = currdf[:, 1:]
     data_x = curr_df1[:, np.array(x_index) - 1]
     data_y = curr_df1[:, np.array(y_index) - 1]
     data_lh = curr_df1[:, np.array(l_index) - 1]
     currdf_filt: np.ndarray = np.zeros((data_x.shape[0] - 1, (data_x.shape[1]) * 2))
 
-    config.bsoid_logger.info('Computing data threshold to forward fill any sub-threshold (x,y)...')
+    logger.info('Computing data threshold to forward fill any sub-threshold (x,y)...')
     for _ in range(data_lh.shape[1]):  # TODO: low: simplify `perc_rect` initialization as list of zeroes
         perc_rect.append(0)
     for x in tqdm(range(data_lh.shape[1])):
-        a, b = np.histogram(data_lh[1:, x].astype(np.float))
-        rise_a = np.where(np.diff(a) >= 0)
+        histogram, bin_edges = np.histogram(data_lh[1:, x].astype(np.float))
+        rise_a = np.where(np.diff(histogram) >= 0)
         if rise_a[0][0] > 1:
-            llh = ((b[rise_a[0][0]] + b[rise_a[0][0]-1]) / 2)
+            llh = ((bin_edges[rise_a[0][0]] + bin_edges[rise_a[0][0]-1]) / 2)
         else:
-            llh = ((b[rise_a[0][1]] + b[rise_a[0][1]-1]) / 2)
+            llh = ((bin_edges[rise_a[0][1]] + bin_edges[rise_a[0][1]-1]) / 2)
         data_lh_float = data_lh[1:, x].astype(np.float)
         perc_rect[x] = np.sum(data_lh_float < llh) / data_lh.shape[0]
         for i in range(1, data_lh.shape[0] - 1):
@@ -164,11 +167,11 @@ def adaptive_filter_data(df_input: pd.DataFrame) -> Tuple[np.ndarray, List]:
                 currdf_filt[i, (2 * x):(2 * x + 2)] = np.hstack([data_x[i, x], data_y[i, x]])
     currdf_filt = np.array(currdf_filt[1:])
     currdf_filt = currdf_filt.astype(np.float)
+
     return currdf_filt, perc_rect
 
 
 # Legacy functions. Will be potentially deleted later.
-
 def main(folders: List[str]) -> Tuple[Any, Any, Any]:
     """
     :param folders: list, data folders
