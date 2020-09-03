94cc6f1492a1da8a1190a062d7d7c05aec1cb0fe
Killian Stacey <killianstacey5@gmail.com>
2020-09-03 12:06:55 -0700

    add _doc_ to pipeline

--- bsoid/pipeline.py
+++ bsoid/pipeline.py
@@ -1,7 +1,8 @@
 """
-create data pipelines here
+Create data pipelines here.
 
-
+All steps that go from transforming raw data into producing feature-rich data
+go here, but only the pipeline goes here. Specific implementations go elsewhere.
 """
 
 
