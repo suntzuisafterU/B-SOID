660818dd285775048bb0036eccd1db6025d0ebac
Killian Stacey <killianstacey5@gmail.com>
2020-09-05 11:02:14 -0700

    Add new check_arg function for length

--- bsoid/util/check_arg.py
+++ bsoid/util/check_arg.py
@@ -1,8 +1,20 @@
 import bsoid
 
 
+
+
+
 def ensure_type(var, expected_type):
     """"""
     if not isinstance(var, expected_type):
         type_err = f'For object (value = {var}), expected type was {expected_type} but instead found {type(var)}'
         raise TypeError(type_err)
+
+
+def ensure_collection_not_empty(collection):
+
+    if len(collection) == 0:
+        err = f''
+        raise ValueError(err)
+
+
