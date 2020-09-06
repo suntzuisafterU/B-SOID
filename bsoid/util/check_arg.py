ea5faed097a6b9aa32ec73f01250e49acdeaa471
Killian Stacey <killianstacey5@gmail.com>
2020-09-05 17:41:13 -0700

    check_arg error message updated and file reformatted

--- bsoid/util/check_arg.py
+++ bsoid/util/check_arg.py
@@ -1,20 +1,19 @@
 import bsoid
 
 
-
-
+###
 
 def ensure_type(var, expected_type):
     """"""
     if not isinstance(var, expected_type):
         type_err = f'For object (value = {var}), expected type was {expected_type} but instead found {type(var)}'
+        # logger.error(err)
         raise TypeError(type_err)
 
 
 def ensure_collection_not_empty(collection):
-
+    """"""
     if len(collection) == 0:
-        err = f''
+        err = f'Input variable was expected to be non-empty but was in fact empty. Value = {collection}.'
+        # logger.error(err)
         raise ValueError(err)
-
-
