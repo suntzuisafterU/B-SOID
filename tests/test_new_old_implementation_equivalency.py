2387e7bc557e2dddf758faf73f7db3eed56a177f
Killian Stacey <killianstacey5@gmail.com>
2020-09-05 18:39:39 -0700

    Reorganize test logic

--- tests/test_new_old_implementation_equivalency.py
+++ tests/test_new_old_implementation_equivalency.py
@@ -23,14 +23,13 @@ class TestNewFunctionEquivalencyToLegacy(TestCase):
 
         """
         # Arrange
-        # # 1/2: Set up data for function use
-        body_parts, fps = bsoid.config.BODYPARTS_PY_LEGACY, bsoid.config.VIDEO_FPS
-
-        df_input_data = pd.read_csv(test_file_location_7_features, nrows=bsoid.config.max_rows_to_read_in_from_csv)
-        data_as_array, _ = bsoid.util.likelihoodprocessing.process_raw_data_and_filter_adaptively(df_input_data)
-        # # 2/2: Tee up functions to be compared
+        # # 1/2: Tee up functions to be compared
         bsoid_py_extract_function__as_is: callable = bsoid.classify.bsoid_extract_features_py_without_assuming_100ms_bin_integration
         new_feature_extraction_function: callable = bsoid.feature_engineering.extract_7_features_bsoid_tsne_py
+        # # 2/2: Set up data for function use
+        body_parts, fps = bsoid.config.BODYPARTS_PY_LEGACY, bsoid.config.VIDEO_FPS
+        df_input_data = pd.read_csv(test_file_location_7_features, nrows=bsoid.config.max_rows_to_read_in_from_csv)
+        data_as_array, _ = bsoid.util.likelihoodprocessing.process_raw_data_and_filter_adaptively(df_input_data)
 
         # Act
         features_output_original_function: List[np.ndarray] = bsoid_py_extract_function__as_is([data_as_array], body_parts, fps)
@@ -57,13 +56,15 @@ new output array: {features_output_new_function}
         self.assertTrue(is_features_data_output_equal, msg=arrays_not_equal_err)
 
     def test__legacy_bsoid_extract_has_same_output_as_functionally_segregated_equivalent(self):
-        """The original implementation for bsoid_extract (_py submodule) assumed that the user wants features
-        further filtered to 100ms bins. The original function was separated into 2 new functions and
+        """
+        The original implementation for bsoid_extract (_py submodule) assumed that the user wants features
+        further filtered to 100ms bins. In response, the original function was separated into 2 new functions and
         this test aims to confirm the correctness that pipelining data from the first and second new functions is
-        equivalent to using the original implementation."""
+        equivalent to using the original implementation.
+        """
         # Arrange
         # # 1/2: Tee up functions to be compared
-        bsoid_py_extract_function__as_is = bsoid.classify.bsoid_extract_py
+        bsoid_py_extract_function__original = bsoid.classify.bsoid_extract_py
 
         first_extraction_seg_func = bsoid.feature_engineering.extract_7_features_bsoid_tsne_py
         second_extraction_seg_func = bsoid.classify.integrate_features_into_100ms_bins
@@ -80,9 +81,11 @@ new output array: {features_output_new_function}
 
         # Act
         # # Get outcome of original function
-        features_output_original_function: List[np.ndarray] = bsoid_py_extract_function__as_is([data_as_array], body_parts, fps)
-        # # Get outcome of new segregated functions
-        features_output_new_function_1_of_2_done: List[np.ndarray] = first_extraction_seg_func([data_as_array], body_parts, fps)
+        features_output_original_function: List[np.ndarray] = bsoid_py_extract_function__original(
+            [data_as_array, ], body_parts, fps)
+        # # Get outcomes of new segregated functions
+        features_output_new_function_1_of_2_done: List[np.ndarray] = first_extraction_seg_func(
+            [data_as_array, ], body_parts, fps)
         features_output_new_function = second_extraction_seg_func(
             [data_as_array, ], features_output_new_function_1_of_2_done, fps)
 
@@ -111,7 +114,7 @@ new output array: {features_output_new_function}
         # Arrange
         # # 1/2: Tee up functions to be compared
         bsoid_py_extract_function__as_is = bsoid.classify.bsoid_extract_py
-        new_datapreprocess_function = bsoid.classify.bsoid_extract_py
+        new_data_preprocess_function = bsoid.classify.bsoid_extract_py
 
         # # 2/2: Set up data for function use
         body_parts, fps = bsoid.config.BODYPARTS_PY_LEGACY, bsoid.config.VIDEO_FPS
@@ -119,36 +122,35 @@ new output array: {features_output_new_function}
         data_as_array, _ = bsoid.util.likelihoodprocessing.process_raw_data_and_filter_adaptively(df_input_data)
 
         # Act
-        features_output_original_function: List[np.ndarray] = bsoid_py_extract_function__as_is(
-            [data_as_array], body_parts, fps)
-        features_output_new_function: List[np.ndarray] = new_datapreprocess_function(
-            [data_as_array], body_parts, fps)
+        original_function_features_output: List[np.ndarray] = bsoid_py_extract_function__as_is(
+            [data_as_array, ], body_parts, fps)
+        new_function_features_output: List[np.ndarray] = new_data_preprocess_function(
+            [data_as_array, ], body_parts, fps)
 
         # Assert (Note: usually multiple asserts in a single test is bad form, but we can refactor this test later)
         # 1/2: Assert types first
-        is_old_output_list = isinstance(features_output_original_function, list)
-        is_new_output_list = isinstance(features_output_new_function, list)
+        is_old_output_list = isinstance(original_function_features_output, list)
         self.assertTrue(is_old_output_list)
+        is_new_output_list = isinstance(new_function_features_output, list)
         self.assertTrue(is_new_output_list)
-        # # 2/2: Assert outcomes are equal second
 
-        is_features_data_output_equal = False not in [(a1 == a2).all() for a1, a2 in
-                                                      zip(features_output_original_function,
-                                                          features_output_new_function)]
+        # # 2/2: Assert outcomes are equal second
+        is_features_data_output_equal = False not in [
+            (a1 == a2).all() for a1, a2 in zip(original_function_features_output, new_function_features_output)]
         arrays_not_equal_err = f"""
 Arrays not identical.
-original output array shape: {features_output_original_function[0].shape}
-new output array shape: {features_output_new_function[0].shape}
+original output array shape: {original_function_features_output[0].shape}
+new output array shape: {new_function_features_output[0].shape}
 
-original output array: {features_output_original_function}
-new output array: {features_output_new_function}
+original output array: {original_function_features_output}
+new output array: {new_function_features_output}
 """.strip()
         self.assertTrue(is_features_data_output_equal, msg=arrays_not_equal_err)
 
     @skip
     def test__old_vs_new_feature_extraction__bsoid_voc(self):
         """
-        TODO: Finish implementation.
+        TODO: Finish test implementation.
             - Needs a proper file to use as test data
             - Needs to have "new" function correctly segregated and added as the function to test against
         """
