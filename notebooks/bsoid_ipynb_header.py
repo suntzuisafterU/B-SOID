d0456e887a01941370a9ef7c13481855ebdf5103
Killian Stacey <killianstacey5@gmail.com>
2020-10-14 17:33:58 -0700

    Add ipnyb to repo for reference

--- notebooks/bsoid_ipynb_header.py
+++ notebooks/bsoid_ipynb_header.py
@@ -3,3 +3,4 @@ import os; import sys
 bsoid_PROJECT_path = os.path.dirname(os.path.dirname(os.path.abspath(__name__)))
 if bsoid_PROJECT_path not in sys.path:
     sys.path.insert(0, bsoid_PROJECT_path)
+# from bsoid.pipeline import *  # This line is required for Streamlit to load Pipeline objects. Do not delete. For a more robust solution, consider: https://rebeccabilbro.github.io/module-main-has-no-attribute/
