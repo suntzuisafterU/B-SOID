29ae16ecf1feb2e175040a77664ddb933357bb8e
Killian Stacey <killianstacey5@gmail.com>
2020-10-28 12:28:35 -0700

    test_pipeline: naming updates

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -19,9 +19,10 @@ assert os.path.isfile(csv_test_file_path)
 class TestPipeline(TestCase):
 
     # SCALING DATA
+    @skip   # Temporary skip since it takes forever
     def test__scale_data__shouldReturnDataFrameWithSameColumnNames__afterScalingData(self):
         # Arrange
-        p = bsoid.pipeline.PipelinePrime('TestPipeline6546546549879876546468').add_predict_data_source(csv_test_file_path).build(True)
+        p = bsoid.pipeline.PipelinePrime('TestPipe987dfdp').add_train_data_source(csv_test_file_path).build(True)
 
         # Act
         p = p.scale_transform_train_data()
@@ -40,19 +41,19 @@ scaled_features_cols = {scaled_features_cols}
 Symmetric diff = {unscaled_features_cols.symmetric_difference(scaled_features_cols)}
 
 """.strip()
-        self.assertTrue(is_equal, err_message)
+        self.assertEqual(unscaled_features_cols, scaled_features_cols, err_message)
 
     # Adding new training data sources
     def test__pipeline_adding_train_data_file_source__should____(self):
         """"""
         # Arrange
         data_source_file_path = csv_test_file_path
-        p = bsoid.pipeline.PipelinePrime('TestPipeline_asdf')
-        num_of_sources_before_addition: int = len(p._dfs_list_raw_train_data)
+        pipe = bsoid.pipeline.PipelinePrime('TestPipeline_asdf')
+        num_of_sources_before_addition: int = len(pipe._dfs_list_raw_train_data)
         num_of_sources_should_be_this_after_addition = num_of_sources_before_addition + 1
 
         # Act
-        p = p.add_train_data_source(data_source_file_path)
+        p = pipe.add_train_data_source(data_source_file_path)
         num_of_sources_actually_this_after_addition: int = len(p._dfs_list_raw_train_data)
 
         # Assert
@@ -69,7 +70,7 @@ list_of_sources_after_addition = {num_of_sources_actually_this_after_addition}
     def test__pipeline_adding_train_data_file_source__shouldBeZeroToStart(self):
         """"""
         # Arrange
-        p = bsoid.pipeline.PipelinePrime('Test_65465465465')
+        p = bsoid.pipeline.PipelinePrime('Test_6546df5465465')
         expected_amount_of_dataframes = 0
 
         # Act
@@ -80,14 +81,15 @@ list_of_sources_after_addition = {num_of_sources_actually_this_after_addition}
 expected_amount_of_dataframes = {expected_amount_of_dataframes}
 actual_amount_of_dataframes = {actual_amount_of_dataframes}
 
-"""  # dfs = {[x.to_string() for x in pipeline_65465465465._dfs_list_raw_train_data]}
+df (head) =  \n{[str(len(df))+'___'+df.to_string() for df in p._dfs_list_raw_train_data]}
+"""
         self.assertEqual(expected_amount_of_dataframes, actual_amount_of_dataframes, err_msg)
 
     @skip  # TODO: review if test completely built
     def test__pipeline_adding_train_data_file_source__shouldAddParticularFileTo____when____(self):
         """"""
         # Arrange
-        p = bsoid.pipeline.PipelinePrime('Test123123123')
+        p = bsoid.pipeline.PipelinePrime('Test1231asdf23123')
         data_source_file_path = csv_test_file_path
         self.assertTrue(data_source_file_path not in p.train_data_files_paths)
 
@@ -106,7 +108,7 @@ p.train_data_files_paths = {p.train_data_files_paths}
 
         """
         # Arrange
-        p = bsoid.pipeline.PipelinePrime('Test_65465465465asdfasdfde34asdf')
+        p = bsoid.pipeline.PipelinePrime('Test_65465465465asddsfasdfde34asdf')
         num_sources_before_adding_any = len(p.train_data_files_paths)
 
         # Act
@@ -129,7 +131,7 @@ p.train_data_files_paths = {p.train_data_files_paths}
 
         """
         # Arrange
-        p = bsoid.pipeline.PipelinePrime()
+        p = bsoid.pipeline.PipelinePrime('asdfasdfdfs44444')
         p = p.add_train_data_source(csv_test_file_path)
         original_number_of_data_rows = len(bsoid.read_csv(csv_test_file_path))
 
@@ -141,7 +143,7 @@ p.train_data_files_paths = {p.train_data_files_paths}
         err_msg = f'TODO: err msg'
         self.assertEqual(original_number_of_data_rows, actual_total_rows_after_feature_engineering, err_msg)
 
-    def test__stub3(self):
+    def test__stub3(self):  # TODO: rename func for accuracy
         """
         Test to see if output is None if no assignment label found
         """
