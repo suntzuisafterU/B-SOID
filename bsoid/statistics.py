264b82564350d7caeed2aa0588fd8e0fd598e66e
Killian Stacey <killianstacey5@gmail.com>
2020-11-09 12:44:44 -0800

    statistics.py: add windowing functions to bottom

--- bsoid/statistics.py
+++ bsoid/statistics.py
@@ -246,3 +246,66 @@ def behv_dur(labels) -> Tuple[pd.DataFrame, pd.DataFrame]:
     df_dur_statistics = pd.DataFrame(all_duration_data_as_array, columns=duration_statistics_columns)
 
     return df_runlengths, df_dur_statistics
+
+
+### Aaron misc averaging funcs
+def windower(window_size, analysisfn, myplay):
+
+    xs = []
+    ys = []
+    for i in range(len(myplay) - window_size):
+        myslice = myplay[i:i+window_size]
+        ys.append(analysisfn(myslice))
+        xs.append(time_average(myslice))
+
+    return xs, ys
+
+
+def windower3d(window_size, analysisfn, myplay):
+    xs = []
+    ys = []
+    zs = []
+    for i in range(len(myplay) - window_size):
+        myslice = myplay[i:i+window_size]
+        zs.append(analysisfn(myslice))
+        ys.append(averageY(myslice))
+        xs.append(averageX(myslice))
+
+    return xs, ys, zs
+
+
+def time_average(slicein):
+    time = 0
+    for i in slicein:
+        time += i["EventTime"]
+    return time / len(slicein)
+
+
+def averageX(slicein):
+    xpos = 0
+    for i in slicein:
+        xpos += i["EventOrigin_x"]
+    return xpos / (len(slicein) + 1)
+
+
+def averageY(slicein):
+    ypos = 0
+    for i in slicein:
+        ypos += i["EventOrigin_y"]
+    return ypos / (len(slicein) + 1)
+
+
+def smoothing(pts, dist):
+    newpts = []
+    for i in range(len(pts) - len(dist)):
+        newpt = 0
+        for j in range(len(dist)):
+            newpt += pts[i + j] * dist[j]
+        newpts.append(newpt)
+
+    return newpts
+
+
+def gaussian(mu, sig, size):
+    x = np.linspace(-3, 3, size)
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
