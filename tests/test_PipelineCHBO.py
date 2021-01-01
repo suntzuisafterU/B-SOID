676baff69277a882ab6c1ef70f0acd95115f09fd
Killian Stacey <killianstacey5@gmail.com>
2021-01-01 11:29:24 -0800

    Add test file: test_PipelineCHBO

---
+++ tests/test_PipelineCHBO.py
@@ -0,0 +1,46 @@
+"""
+
+"""
+from typing import Any, Dict, List, Set
+from unittest import TestCase, skip
+import itertools
+import numpy as np
+import os
+import pandas as pd
+import random
+import time
+
+import bsoid
+
+# TODO: change from Prime CSV to NEW csv with proper headers
+csv_test_file_path = bsoid.config.DEFAULT_PIPELINE__PRIME__CSV_TEST_FILE_PATH
+assert os.path.isfile(csv_test_file_path)
+
+
+# TODO: add test for pipeline.set_params()
+class TestPipelineCHBO(TestCase):
+
+    @skip
+    def test__stub7(self):
+        """
+
+        """
+        # Arrange
+
+        # Act
+        is_equal = 1 + 1 == 2
+        # Assert
+        self.assertTrue(is_equal)
+
+    @skip
+    def test__stub8(self):
+        """
+
+        """
+        # Arrange
+
+        # Act
+        is_equal = 1 + 1 == 2
+        # Assert
+        self.assertTrue(is_equal)
+
